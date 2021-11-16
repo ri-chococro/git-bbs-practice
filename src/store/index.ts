1e32095e87f7c46480f5d594dd574da114ab5a36
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-16 15:04:49 +0900

    記事一覧を表示

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,25 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { Article } from "@/types/article";
+import { Comment } from "@/types/comment";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    state: {
-      articles: [
-        new Article(3, "佐藤", "佐藤さんの記事", []),
-        new Article(2, "⼭⽥", "⼭⽥さんの記事", [
-          new Comment(13, "⼭崎", "⼭崎さんのコメント", 2),
-        ]),
-        new Article(1, "伊藤", "伊藤さんの記事", [
-          new Comment(12, "鈴⽊", "鈴⽊さんのコメント", 1),
-          new Comment(11, "吉⽥", "吉⽥さんのコメント", 1),
-        ]),
-      ],
-    },
+    articles: [
+      new Article(3, "佐藤", "佐藤さんの記事", []),
+      new Article(2, "⼭⽥", "⼭⽥さんの記事", [
+        new Comment(13, "⼭崎", "⼭崎さんのコメント", 2),
+      ]),
+      new Article(1, "伊藤", "伊藤さんの記事", [
+        new Comment(12, "鈴⽊", "鈴⽊さんのコメント", 1),
+        new Comment(11, "吉⽥", "吉⽥さんのコメント", 1),
+      ]),
+    ],
   },
   mutations: {},
   actions: {},
-  getters: {},
+  getters: {
+    getArticles(state) {
+      return state.articles;
+    },
+  },
   modules: {},
 });
