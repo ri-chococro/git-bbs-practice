496bb30b51503ccf37f7fae9441fa9b533f64b49
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-16 14:02:17 +0900

    router,store,appを初期設定

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    state: {
+      articles: [
+        new Article(3, "佐藤", "佐藤さんの記事", []),
+        new Article(2, "⼭⽥", "⼭⽥さんの記事", [
+          new Comment(13, "⼭崎", "⼭崎さんのコメント", 2),
+        ]),
+        new Article(1, "伊藤", "伊藤さんの記事", [
+          new Comment(12, "鈴⽊", "鈴⽊さんのコメント", 1),
+          new Comment(11, "吉⽥", "吉⽥さんのコメント", 1),
+        ]),
+      ],
+    },
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  getters: {},
+  modules: {},
+});
